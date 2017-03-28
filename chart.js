0203fbf123a735d88c084ca0028cf23e4d0d3a74
John Clark <johngclark36@gmail.com>
2017-03-28 16:17:56 -0400

    Setup scaling for x and y axes

--- chart.js
+++ chart.js
@@ -14,6 +14,29 @@ $(document).ready(function () {
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
 
+    //setup x and y scales
 
+    var firstPlace = result[0].Seconds
+    var lastPlace = result[34].Seconds
+    var secondsGap = firstPlace - lastPlace
+
+    var xScale = d3.scale.linear()
+      .domain([secondsGap, 0])
+      .range([0, width])
+
+    var yScale = d3.scale.linear()
+      .domain([0, 35])
+      .range([height, 0])
+
+    var xAxis = d3.svg.axis()
+      .scale(xScale)
+      .orient("left")
+
+    var yAxis = d3.svg.axis()
+      .scale(yScale)
+      .orient("bottom")
+      .ticks(8, "")
   })
+
+  
 });
